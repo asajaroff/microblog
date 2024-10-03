09cebb82b68f47b806bbbc67c912503fc7648d0d
Alejandro Sajaroff <29068982+asajaroff@users.noreply.github.com>
2024-10-03 11:51:54 +0200

    feat:  - README.org  - sql/01_init.sql  - sql/02_users.sql  - gitignore

---
+++ sql/02_users.sql
@@ -0,0 +1,14 @@
+CREATE TABLE users IF NOT EXISTS (
+    id BIGSERIAL PRIMARY KEY,
+    username VARCHAR(150) NOT NULL UNIQUE,
+    email VARCHAR(254) NOT NULL UNIQUE,
+    encrypted_password VARCHAR(255) NOT NULL,
+    first_name VARCHAR(150),
+    last_name VARCHAR(150),
+    is_admin BOOL DEFAULT FALSE,
+    sign_in_count INTEGER DEFAULT 0,
+    falined_sign_in_count INTEGER DEFAULT 0,
+    current_sign_in_at TIMESTAMP,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
